3b3cf9bca7ae215353c4497b1b28638e32cb9f40
Andre <santana.agr@gmail.com>
2021-07-13 13:52:21 -0300

    coloquei segundo efeito

--- src/CompsNStyles/BottomContainer.js
+++ src/CompsNStyles/BottomContainer.js
@@ -1,11 +1,14 @@
 import '../styles.css'
 import EfeitoUm from './EfeitoUm'
+import EfeitoDois from './EfeitoDois'
+
+////////////////////////////////////
 
 export default function BottomContainer() {
   return (
     <div className="BottomContainer">
       <EfeitoUm></EfeitoUm>
-      <EfeitoUm></EfeitoUm>
+      <EfeitoDois></EfeitoDois>
       <EfeitoUm></EfeitoUm>
       <EfeitoUm></EfeitoUm>
       <EfeitoUm></EfeitoUm>
---
+++ src/CompsNStyles/EfeitoDois.js
@@ -0,0 +1,17 @@
+import '../styles.css'
+import img2 from '../imgs/img2.jpg'
+
+export default function EfeitoDois() {
+  return (
+    <div className="EfeitoDois">
+      <img src={img2} alt="imagem 1" />
+      <div>
+        <h3>Caption Title</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+          quibusdam sunt alias mollitia quos atque.
+        </p>
+      </div>
+    </div>
+  )
+}
