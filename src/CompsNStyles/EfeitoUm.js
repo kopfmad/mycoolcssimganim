247f4c3cef26486328ec165ed14d97e4c755d79e
Andre <santana.agr@gmail.com>
2021-07-13 13:26:36 -0300

    adicionando o EfeitoUm

--- src/CompsNStyles/EfeitoUm.js
+++ src/CompsNStyles/EfeitoUm.js
@@ -1,5 +1,17 @@
 import '../styles.css'
+import img1 from '../imgs/img1.jpg'
 
 export default function EfeitoUm() {
-  return <div className="EfeitoUm"></div>
+  return (
+    <div className="EfeitoUm">
+      <img src={img1} alt="imagem 1" />
+      <div>
+        <h3>Caption Title</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+          quibusdam sunt alias mollitia quos atque.
+        </p>
+      </div>
+    </div>
+  )
 }
