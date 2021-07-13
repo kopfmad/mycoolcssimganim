d1254de1d76584065deea53feaa27bbd6616e274
Andre <santana.agr@gmail.com>
2021-07-13 08:46:14 -0300

    Adicionei a estrutura básica, agora falta criar cada efeito

--- src/CompsNStyles/BottomContainer.js
+++ src/CompsNStyles/BottomContainer.js
@@ -1,5 +1,18 @@
 import '../styles.css'
+import EfeitoUm from './EfeitoUm'
 
-export default function BottomContainer(props) {
-  return <div className="BottomContainer">{props.children}</div>
+export default function BottomContainer() {
+  return (
+    <div className="BottomContainer">
+      <EfeitoUm></EfeitoUm>
+      <EfeitoUm></EfeitoUm>
+      <EfeitoUm></EfeitoUm>
+      <EfeitoUm></EfeitoUm>
+      <EfeitoUm></EfeitoUm>
+      <EfeitoUm></EfeitoUm>
+      <EfeitoUm></EfeitoUm>
+      <EfeitoUm></EfeitoUm>
+      <EfeitoUm></EfeitoUm>
+    </div>
+  )
 }
---
+++ src/CompsNStyles/EfeitoUm.js
@@ -0,0 +1,5 @@
+import '../styles.css'
+
+export default function EfeitoUm() {
+  return <div className="EfeitoUm"></div>
+}
